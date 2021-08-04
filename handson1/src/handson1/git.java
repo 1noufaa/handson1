3104f895542a0d445aaebef9a110ffb8bf5fbe36
923387 <923387@LTSA000369.cts.com>
2021-08-05 01:29:48 +0300

    sent

--- handson1/src/handson1/git.java
+++ handson1/src/handson1/git.java
@@ -3,8 +3,8 @@ package handson1;
 public class git {
 
 	public void method1(){
-		System.out.println("testing 1");
-	}
+		System.out.println("testing 1");}
+	
 	public void method2(){
-		System.out.println("testing 2");
+		System.out.println("testing 2");}
 }
